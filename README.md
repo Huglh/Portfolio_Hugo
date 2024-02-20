9dcd39c6e92f2f5cc9cea186077e31eaf830be0b
Hugo Thiago <103154103+Huglh@users.noreply.github.com>
2024-02-20 15:43:09 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio_Hugo
+Meu portfólio destaca minhas habilidades e conquistas. Demonstro minha paixão pela área e meu compromisso com resultados excepcionais. Uma vitrine poderosa para atrair oportunidades alinhadas aos meus objetivos.
+  <div style="display: inline_block"><br>
+     <a href="#"><img src="https://cdn.discordapp.com/attachments/731999769378947182/1209565274349510666/image.png?ex=65e76272&is=65d4ed72&hm=d3cf9630dfe653ed1017ccf5c365e18bbcd0cec6fa3c35d9c57b81a5f432821e&" alt="PDF-Ve-culos" border="0" /></a>
+  </div>
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Ative o bootstrap scrollspy no elemento de navegação principal
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Recolher a barra de navegação responsiva quando o alternador estiver visível
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
